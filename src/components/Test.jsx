e6d5a4edc71a1b46bfc934e8a598cf7f706926b9
sowmyareddy705 <23r11a6705@gcet.edu.in>
2025-05-22 20:28:34 +0530

    UPDATE

---
+++ src/components/Test.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function () {
+  return (
+    <div>
+      HEY!! Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat corrupti aspernatur, aliquid mollitia dolores dolorem quae distinctio minima laudantium libero adipisci dolor blanditiis odit voluptates a dicta quod est laboriosam?
+    </div>
+  );
+}
