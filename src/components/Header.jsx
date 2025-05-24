01a6ad93c2e0cdbd73f0f56b8d873ac2b91d1156
sowmyareddy705 <23r11a6705@gcet.edu.in>
2025-05-24 22:42:10 +0530

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./Header.css"
+import "./Header.css";
 export default function Header() {
   return (
     <div className="App-Header-Row">
-      <h1 style={{backgroundColor:'yellow',color:'blue'}}>React Lessons</h1>
-      <Link to="/">App1</Link>-<Link to="/app2">App2</Link>
+      <h1 style={{ backgroundColor: "yellow", color: "blue" }}>
+        React Lessons
+      </h1>
+      <Link to="/">App1</Link>-<Link to="/app2">App2</Link>-
+      <Link to="/app3">App3</Link>-
+      <Link to="/app4">App4</Link>-
+      <Link to="/app5">App5</Link>-
+      <Link to="/app6">App6</Link>-
+      <Link to="/app7">App7</Link>
     </div>
   );
 }
