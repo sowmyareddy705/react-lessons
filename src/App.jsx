ffd8c95179bdaeb56451be11ee451a6f7925f26a
sowmyareddy705 <23r11a6705@gcet.edu.in>
2025-05-25 21:02:20 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,6 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import "./App.css";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import App10 from "./components/App10";
 function App() {
   return (
     <BrowserRouter>
