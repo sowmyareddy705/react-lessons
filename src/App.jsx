439ee78c83c79973d3e2871211bd8bb858ff85d3
sowmyareddy705 <23r11a6705@gcet.edu.in>
2025-05-27 19:10:48 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -18,8 +18,8 @@ import App14 from "./components/App14";
 import App15 from "./components/App15";
 import App16 from "./components/App16";
 import App17 from "./components/App17";
-import App17 from "./components/App18";
-import App17 from "./components/App19";
+import App18 from "./components/App18";
+import App19 from "./components/App19";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
