0b67cc9563ae39c733b97c177b4c68d016820b59
sowmyareddy705 <23r11a6705@gcet.edu.in>
2025-05-27 20:19:01 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -20,8 +20,10 @@ import App16 from "./components/App16";
 import App17 from "./components/App17";
 import App18 from "./components/App18";
 import App19 from "./components/App19";
+import App20 from "./components/App20";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+
 import "./App.css";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 function App() {
@@ -46,9 +48,10 @@ function App() {
           <Route path="/app14" element={<App14 />}></Route>
           <Route path="/app15" element={<App15 />}></Route>
            <Route path="/app16" element={<App16 />}></Route>
-           <Route path="/app17" element={<App17 />}></Route>
-           <Route path="/app18" element={<App18 />}></Route>
-            <Route path="/app19" element={<App19 />}></Route>
+            <Route path="/app17" element={<App17 />}></Route>
+             <Route path="/app18" element={<App18 />}></Route>
+             <Route path="/app19" element={<App19 />}></Route>
+             <Route path="/app20" element={<App20 />}></Route>
       </Routes>
       <Footer />
     </BrowserRouter>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,8 @@ export default function Header() {
       <h1 style={{ backgroundColor: "yellow", color: "blue" }}>
         React Lessons
       </h1>
-      <Link to="/">App1</Link>-<Link to="/app2">App2</Link>-
+      <Link to="/">App1</Link>-
+      <Link to="/app2">App2</Link>-
       <Link to="/app3">App3</Link>-
       <Link to="/app4">App4</Link>-
       <Link to="/app5">App5</Link>-
@@ -18,13 +19,14 @@ export default function Header() {
       <Link to="/app10">App10</Link>-
       <Link to="/app11">App11</Link>-
       <Link to="/app12">App12</Link>-
-      <Link to="/app13">App13</Link>-
-      <Link to="/app14">App14</Link>-
-      <Link to="/app15">App15</Link>-
-      <Link to="/app16">App16</Link>-
-      <Link to="/app17">App17</Link>-
-      <Link to="/app18">App18</Link>-
-      <Link to="/app19">App19</Link>-
+       <Link to="/app13">App13</Link>-
+        <Link to="/app14">App14</Link>-
+         <Link to="/app15">App15</Link>-
+         <Link to="/app16">App16</Link>-
+         <Link to="/app17">App17</Link>-
+          <Link to="/app18">App18</Link>-
+           <Link to="/app19">App19</Link>-
+            <Link to="/app20">App20</Link>
     </div>
   );
 }
